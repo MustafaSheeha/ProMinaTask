b91b12794149695517816025f50caaa7480378f2
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 15:28:59 +0300

    Add white and black colors to AppColors class

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -4,4 +4,6 @@ abstract class AppColors {
   static const lavenderBlue= Color(0xffDDCDFF);
   static const lavenderBlush= Color(0xffEBE1FF);
   static const orchid=Color(0xffEA94D7);
+  static const white=Color(0xffffffff);
+  static const black=Color(0xff000000);
 }
