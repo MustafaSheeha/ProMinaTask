c893153843cce2a95d1fae0ae8a7f8c343e04645
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 22:23:00 +0300

    Add password field to ApiKey class

--- lib/core/api/api_key.dart
+++ lib/core/api/api_key.dart
@@ -3,6 +3,7 @@ abstract class ApiKey {
   static const id='id';
   static const name='name';
   static const email='email';
+  static const password='password';
   static const emailVerifiedAt='email_verified_at';
   static const createdAt='created_at';
   static const updatedAt='updated_at';
