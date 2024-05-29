7cf20646f48fcb46f48ed47c25fd66918a8b2cb1
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 15:08:53 +0300

    Add AppColors and AppStrings constants.

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,12 @@
+abstract class AppStrings {
+  static const my = 'My';
+  static const gellary = 'Gallery';
+  static const login = 'LOG IN';
+  static const userName = 'User Name';
+  static const password = 'Password';
+  static const submit = 'SUBMIT';
+  static const welcome = 'Welcome';
+  static const logout = 'log out';
+  static const upload = 'upload';
+  static const camera = 'Camera';
+}
