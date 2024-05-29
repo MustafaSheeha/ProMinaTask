7042981c30d003e504d5304becc0e4d82b7ac9b6
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 20:44:25 +0300

    Add ApiKey and EndPoints classes.

---
+++ lib/core/api/api_key.dart
@@ -0,0 +1,11 @@
+abstract class ApiKey {
+  static const user='user';
+  static const id='id';
+  static const name='name';
+  static const email='email';
+  static const emailVerifiedAt='email_verified_at';
+  static const createdAt='created_at';
+  static const updatedAt='updated_at';
+  static const token='token';
+  static const errorMessage='error_message';
+}
---
+++ lib/core/api/end_points.dart
@@ -0,0 +1,6 @@
+abstract class EndPoints {
+  static const baseUrl='https://flutter.prominaagency.com/api/';
+  static const login='auth/login';
+  static const upload='upload';
+  static const myGallery='my-gallery';
+}
