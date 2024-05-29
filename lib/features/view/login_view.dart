50c6fc0115d8c27932522d418f4cfce772a41915
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 17:25:54 +0300

    Add LoveCamerPhotographyWidget and update LoginView UI.

--- lib/features/view/login_view.dart
+++ lib/features/view/login_view.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:promina_gallery_task/core/constants/app_colors.dart';
+import 'package:promina_gallery_task/core/constants/app_strings.dart';
+import 'package:promina_gallery_task/core/constants/app_text_styles.dart';
 import 'package:promina_gallery_task/core/constants/assets.dart';
 
 import 'widgets/curved_circle_list_widget.dart';
 import 'widgets/curved_circle_widget.dart';
 import 'widgets/gradient_container.dart';
+import 'widgets/love_camera_photography_widget.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Stack(
         children: [
           GradientContainer(),
           CurvedCircleListWidget(),
+          LoveCamerPhotographyWidget(),
+          SizedBox(
+            width: Get.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  AppStrings.myGellary,
+                  textAlign: TextAlign.center,
+                  style: AppTextStyles.segoeUIBold.copyWith(fontSize: 32),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  child: Column(
+                    children: [
+                      Text(
+                        AppStrings.login,
+                        textAlign: TextAlign.center,
+                        style: AppTextStyles.segoeUIBold.copyWith(fontSize: 30),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
---
+++ lib/features/view/widgets/love_camera_photography_widget.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../core/constants/assets.dart';
+
+class LoveCamerPhotographyWidget extends StatelessWidget {
+  const LoveCamerPhotographyWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: Get.height * 0.04,
+      left: Get.width * 0.2,
+      child: Image.asset(Assets.imagesLovePhotography),
+    );
+  }
+}
