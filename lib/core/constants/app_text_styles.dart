e172c91dcd2d21e4dc9b1941f2a2f1cfb4eec8b0
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 18:38:26 +0300

    Add new text style for semi-bold font weight in AppTextStyles class

--- lib/core/constants/app_text_styles.dart
+++ lib/core/constants/app_text_styles.dart
@@ -4,6 +4,11 @@ abstract class AppTextStyles {
   static const  segoeUIBold = TextStyle(
     fontWeight: FontWeight.w700,
   );
+  static const  segoeUISemiBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff988F8C)
+  );
   static const  balooThambi2 = TextStyle(
     fontWeight: FontWeight.w600,
   );
