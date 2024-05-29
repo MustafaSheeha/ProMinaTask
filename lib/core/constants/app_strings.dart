ca0327d7adcbf9154ad69375a33d4f79615afbb0
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 17:25:37 +0300

    Refactor app strings and add app text styles.

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -1,5 +1,5 @@
 abstract class AppStrings {
-  static const my = 'My';
+  static const myGellary = 'My\nGallery';
   static const gellary = 'Gallery';
   static const login = 'LOG IN';
   static const userName = 'User Name';
@@ -9,4 +9,6 @@ abstract class AppStrings {
   static const logout = 'log out';
   static const upload = 'upload';
   static const camera = 'Camera';
+  static const balooThambi2 = 'BalooThambi2';
+  static const segoeUI = 'SegoeUI';
 }
---
+++ lib/core/constants/app_text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const  segoeUIBold = TextStyle(
+    fontWeight: FontWeight.w700,
+  );
+  static const  balooThambi2 = TextStyle(
+    fontWeight: FontWeight.w600,
+  );
+}
