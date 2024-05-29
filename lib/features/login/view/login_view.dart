79f65361addde627530f312b8be1a5930b84f263
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 19:19:41 +0300

    Refactor login view layout and add login form widget.

--- lib/features/login/view/login_view.dart
+++ lib/features/login/view/login_view.dart
@@ -4,11 +4,13 @@ import 'package:promina_gallery_task/core/constants/app_colors.dart';
 import 'package:promina_gallery_task/core/constants/app_strings.dart';
 import 'package:promina_gallery_task/core/constants/app_text_styles.dart';
 import 'package:promina_gallery_task/core/constants/assets.dart';
+import 'package:promina_gallery_task/core/widgets/vertical_space.dart';
 
 import '../../../core/widgets/text_widget.dart';
 import 'widgets/curved_circle_list_widget.dart';
 import 'widgets/curved_circle_widget.dart';
 import 'widgets/gradient_container.dart';
+import 'widgets/login_form_widget.dart';
 import 'widgets/love_camera_photography_widget.dart';
 import 'widgets/submit_button_widget.dart';
 import 'widgets/text_form_field_widget.dart';
@@ -19,44 +21,56 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          const GradientContainer(),
-          const CurvedCircleListWidget(),
-          const LoveCamerPhotographyWidget(),
-          SizedBox(
-            width: Get.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextWidget(
-                  text: AppStrings.myGellary,
-                  style: AppTextStyles.segoeUIBold.copyWith(fontSize: 32),
-                ),
-                Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 40, horizontal: 42),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(top: 48, left: 52, right: 52),
-                        child: TextWidget(
-                          text: AppStrings.login,
-                          style:
-                              AppTextStyles.segoeUIBold.copyWith(fontSize: 30),
-                        ),
-                      ),
-                      const TextFormFieldWidget(hintText: AppStrings.userName),
-                      const TextFormFieldWidget(hintText: AppStrings.password),
-                      const SubmitButtonWidget()
-                    ],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            const GradientContainer(),
+            const CurvedCircleListWidget(),
+            const LoveCamerPhotographyWidget(),
+            Positioned(
+                top: Get.height * 0.4,
+                left: Get.width * 0.1,
+                child: Image.asset(Assets.imagesRectangle12)),
+            Positioned(
+                top: Get.height * 0.43,
+                left: Get.width * 0.17,
+                child: Image.asset(Assets.imagesEllipse62)),
+            Positioned(
+                top: Get.height * 0.47,
+                left: Get.width * 0.1,
+                child: Image.asset(Assets.imagesPolygon1)),
+            Positioned(
+                top: Get.height * 0.47,
+                left: Get.width * 0.16,
+                child: Image.asset(Assets.imagesPolygon1)),
+            Positioned(
+                top: Get.height * 0.375,
+                left: Get.width * 0.59,
+                child: Image.asset(Assets.imagesEllipse33)),
+            Positioned(
+                top: Get.height * 0.39,
+                left: Get.width * 0.52,
+                child: Image.asset(
+                  Assets.imagesUnion,
+                  scale: 1.6,
+                  color: AppColors.white.withOpacity(0.5),
+                )),
+            Container(
+              margin: EdgeInsets.only(top: Get.height * 0.1),
+              width: Get.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextWidget(
+                    text: AppStrings.myGellary,
+                    style: AppTextStyles.segoeUIBold.copyWith(fontSize: 32),
                   ),
-                )
-              ],
-            ),
-          )
-        ],
+                  const LoginFormWidget()
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
