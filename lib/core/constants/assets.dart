6d9a6fd7ae8e7badc5b2d3cbe7851704b510b886
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 16:45:01 +0300

    Follow Previous Add assets constants for curved circles images.

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,28 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesCurvedCirclesVector2009
+  /// assets/images/curved_circles/Vector_2009.png
+  static const String imagesCurvedCirclesVector2009 = "assets/images/curved_circles/Vector_2009.png";
+
+  /// Assets for imagesCurvedCirclesVector2010
+  /// assets/images/curved_circles/Vector_2010.png
+  static const String imagesCurvedCirclesVector2010 = "assets/images/curved_circles/Vector_2010.png";
+
+  /// Assets for imagesCurvedCirclesVector2011
+  /// assets/images/curved_circles/Vector_2011.png
+  static const String imagesCurvedCirclesVector2011 = "assets/images/curved_circles/Vector_2011.png";
+
+  /// Assets for imagesCurvedCirclesVector2012
+  /// assets/images/curved_circles/Vector_2012.png
+  static const String imagesCurvedCirclesVector2012 = "assets/images/curved_circles/Vector_2012.png";
+
+  /// Assets for imagesCurvedCirclesVector2013
+  /// assets/images/curved_circles/Vector_2013.png
+  static const String imagesCurvedCirclesVector2013 = "assets/images/curved_circles/Vector_2013.png";
+
+  /// Assets for imagesCurvedCirclesVector2014
+  /// assets/images/curved_circles/Vector_2014.png
+  static const String imagesCurvedCirclesVector2014 = "assets/images/curved_circles/Vector_2014.png";
+}
+
