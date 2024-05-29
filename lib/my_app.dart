524770231736ad71ca2403f221ef73bd628c4383
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 15:19:36 +0300

    Add LoginView as the home screen in MyApp.

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'features/view/login_view.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp();
+    return const GetMaterialApp(
+      home: LoginView(),
+    );
   }
 }
