a4bf024d489dcb604b851b188aee8e862f3249e6
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-30 03:29:46 +0300

    Refactor TextWidget to allow for customizable textAlign property

--- lib/core/widgets/text_widget.dart
+++ lib/core/widgets/text_widget.dart
@@ -4,15 +4,16 @@ class TextWidget extends StatelessWidget {
   const TextWidget({
     super.key,
     required this.text,
-    this.style,
+    this.style, this.textAlign=TextAlign.center,
   });
   final String text;
   final TextStyle? style;
+  final TextAlign? textAlign;
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      textAlign: TextAlign.center,
+      textAlign: textAlign,
       style: style,
     );
   }
