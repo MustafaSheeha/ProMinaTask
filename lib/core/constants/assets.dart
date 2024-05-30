968a6427f51fb30f62ffa5dbe8480215035c00e4
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-30 04:30:15 +0300

    Add new images and update assets file.

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -1,29 +1,35 @@
 class Assets {
   Assets._();
-  
+
   /// Assets for imagesCurvedCirclesVector2009
   /// assets/images/curved_circles/Vector_2009.png
-  static const String imagesCurvedCirclesVector2009 = "assets/images/curved_circles/Vector_2009.png";
+  static const String imagesCurvedCirclesVector2009 =
+      "assets/images/curved_circles/Vector_2009.png";
 
   /// Assets for imagesCurvedCirclesVector2010
   /// assets/images/curved_circles/Vector_2010.png
-  static const String imagesCurvedCirclesVector2010 = "assets/images/curved_circles/Vector_2010.png";
+  static const String imagesCurvedCirclesVector2010 =
+      "assets/images/curved_circles/Vector_2010.png";
 
   /// Assets for imagesCurvedCirclesVector2011
   /// assets/images/curved_circles/Vector_2011.png
-  static const String imagesCurvedCirclesVector2011 = "assets/images/curved_circles/Vector_2011.png";
+  static const String imagesCurvedCirclesVector2011 =
+      "assets/images/curved_circles/Vector_2011.png";
 
   /// Assets for imagesCurvedCirclesVector2012
   /// assets/images/curved_circles/Vector_2012.png
-  static const String imagesCurvedCirclesVector2012 = "assets/images/curved_circles/Vector_2012.png";
+  static const String imagesCurvedCirclesVector2012 =
+      "assets/images/curved_circles/Vector_2012.png";
 
   /// Assets for imagesCurvedCirclesVector2013
   /// assets/images/curved_circles/Vector_2013.png
-  static const String imagesCurvedCirclesVector2013 = "assets/images/curved_circles/Vector_2013.png";
+  static const String imagesCurvedCirclesVector2013 =
+      "assets/images/curved_circles/Vector_2013.png";
 
   /// Assets for imagesCurvedCirclesVector2014
   /// assets/images/curved_circles/Vector_2014.png
-  static const String imagesCurvedCirclesVector2014 = "assets/images/curved_circles/Vector_2014.png";
+  static const String imagesCurvedCirclesVector2014 =
+      "assets/images/curved_circles/Vector_2014.png";
 
   /// Assets for imagesEllipse33
   /// assets/images/Ellipse33.png
@@ -33,9 +39,14 @@ class Assets {
   /// assets/images/Ellipse62.png
   static const String imagesEllipse62 = "assets/images/Ellipse62.png";
 
+  /// Assets for imagesLeftward
+  /// assets/images/leftward.png
+  static const String imagesLeftward = "assets/images/leftward.png";
+
   /// Assets for imagesLovePhotography
   /// assets/images/love_photography.png
-  static const String imagesLovePhotography = "assets/images/love_photography.png";
+  static const String imagesLovePhotography =
+      "assets/images/love_photography.png";
 
   /// Assets for imagesPolygon1
   /// assets/images/Polygon1.png
@@ -45,12 +56,23 @@ class Assets {
   /// assets/images/Rectangle12.png
   static const String imagesRectangle12 = "assets/images/Rectangle12.png";
 
+  /// Assets for imagesRedVector
+  /// assets/images/redVector.png
+  static const String imagesRedVector = "assets/images/redVector.png";
+
   /// Assets for imagesUnion
   /// assets/images/Union.png
   static const String imagesUnion = "assets/images/Union.png";
 
+  /// Assets for imagesUpward
+  /// assets/images/upward.png
+  static const String imagesUpward = "assets/images/upward.png";
+
   /// Assets for imagesUserPic
   /// assets/images/userPic.png
   static const String imagesUserPic = "assets/images/userPic.png";
-}
 
+  /// Assets for imagesYellowVector
+  /// assets/images/yellowVector.png
+  static const String imagesYellowVector = "assets/images/yellowVector.png";
+}
