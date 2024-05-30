85b0d317ef6f7aead41e04857598790e87c5cb23
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-30 08:14:11 +0300

    Add image picker functionality to UploadWidget button press

--- lib/features/home/view/widgets/upload_widget.dart
+++ lib/features/home/view/widgets/upload_widget.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:promina_gallery_task/core/constants/assets.dart';
+import 'package:promina_gallery_task/core/widgets/vertical_space.dart';
 
 import '../../../../core/constants/app_colors.dart';
 import '../../../../core/constants/app_strings.dart';
@@ -17,14 +21,79 @@ class UploadWidget extends StatelessWidget {
       style: ButtonStyle(
         backgroundColor: WidgetStateProperty.all(AppColors.white),
       ),
-      onPressed: () {},
+      onPressed: () {
+        Get.dialog(
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 250, horizontal: 32),
+            decoration: BoxDecoration(
+              color: AppColors.white.withOpacity(0.6),
+              borderRadius: BorderRadius.circular(32),
+              backgroundBlendMode: BlendMode.luminosity,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(
+                            AppColors.lightLavenderPink),
+                      ),
+                      onPressed: () async {
+                        await ImagePicker()
+                            .pickImage(source: ImageSource.gallery);
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(Assets.imagesGallery),
+                          TextWidget(
+                            text: AppStrings.gellary,
+                            style: AppTextStyles.segoeUIBold.copyWith(
+                                fontSize: 20, color: AppColors.darkGrey),
+                          ),
+                        ],
+                      )),
+                ),
+                const VerticalSpace(20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStateProperty.all(AppColors.skyBlue),
+                      ),
+                      onPressed: () async {
+                        await ImagePicker()
+                            .pickImage(source: ImageSource.camera);
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(Assets.imagesCamera, scale: 1.8),
+                          TextWidget(
+                            text: AppStrings.camera,
+                            style: AppTextStyles.segoeUIBold.copyWith(
+                                fontSize: 20, color: AppColors.darkGrey),
+                          ),
+                        ],
+                      )),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           const UploadIconWidget(),
           TextWidget(
             text: AppStrings.upload,
-            style: AppTextStyles.balooThambi2.copyWith(fontSize: 20,color: AppColors.darkGrey),
+            style: AppTextStyles.balooThambi2
+                .copyWith(fontSize: 20, color: AppColors.darkGrey),
           )
         ],
       ),
