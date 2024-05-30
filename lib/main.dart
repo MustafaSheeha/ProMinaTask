d0c1e4b71ba7bff298347374ce9c1bd933d85b62
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-30 05:22:50 +0300

    Add CacheHelper initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:promina_gallery_task/core/database/cache/cache_helper.dart';
 import 'package:promina_gallery_task/core/services/api_service.dart';
 import 'package:promina_gallery_task/my_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  CacheHelper().init();
+  Get.put(CacheHelper());
   Get.put(ApiService());
   runApp(const MyApp());
 }
