3dbc906420f97535e9c2de47a663a6685cc52aca
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-30 08:58:08 +0300

    Add system UI overlay style to main.dart
    This commit adds a function to set the system UI overlay style in main.dart. The function sets the status bar color to AppColors.orchid.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:promina_gallery_task/core/constants/app_colors.dart';
 import 'package:promina_gallery_task/core/database/cache/cache_helper.dart';
 import 'package:promina_gallery_task/core/services/api_service.dart';
 import 'package:promina_gallery_task/features/home/controller/home_controller.dart';
@@ -7,9 +9,18 @@ import 'package:promina_gallery_task/my_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  setSystemUIOverlayStyle();
   CacheHelper().init();
   Get.put(CacheHelper());
   Get.put(ApiService());
   Get.put(HomeController());
   runApp(const MyApp());
+}
+
+void setSystemUIOverlayStyle() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: AppColors.orchid,
+    ),
+  );
 }
