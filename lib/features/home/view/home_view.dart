5f1fc08b13ef5c9388f2c4ead39f9218b61e186a
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-30 03:48:40 +0300

    Refactor home view widgets and add LavenderCustomPainter
    The commit refactors the home view widgets by adding a LavenderCustomPainter and three new widgets: UserProfilePicWidget, WelcomeUserWidget, and TextWidget. The LavenderCustomPainter is used to create a custom background for the home view. The UserProfilePicWidget displays the user's profile picture, while the WelcomeUserWidget displays a welcome message and the user's name. The TextWidget is a reusable widget for displaying text. The commit improves the overall structure and organization of the home view widgets.

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:promina_gallery_task/core/constants/app_colors.dart';
+import 'package:promina_gallery_task/core/constants/app_strings.dart';
 import 'package:promina_gallery_task/core/constants/app_text_styles.dart';
+import 'package:promina_gallery_task/core/constants/assets.dart';
+import 'package:promina_gallery_task/core/widgets/text_widget.dart';
+import 'package:promina_gallery_task/core/widgets/vertical_space.dart';
+
+import 'widgets/lavender_custom_painter.dart';
+import 'widgets/user_profile_pic_widget.dart';
+import 'widgets/welcome_user_widget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Home View',
-          style: AppTextStyles.segoeUIBold,
-        ),
+    return Scaffold(
+        body: SafeArea(
+            child: SizedBox(
+      height: Get.height,
+      width: Get.width,
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              WelcomeUserWidget(),
+              UserProfilePicWidget(),
+            ],
+          )
+        ],
       ),
-    );
+    )));
   }
 }
